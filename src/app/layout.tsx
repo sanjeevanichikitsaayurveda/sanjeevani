844089fb17033fd64ae793e8ee64fc7dfcdceafe
sanjeevanichikitsaayurveda <sanjeevanichikitsaayurveda@gmail.com>
2025-03-11 00:02:50 +0530

    update the gt id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+          <iframe src="https://www.googletagmanager.com/ns.html?id=G-Q7YEM590WZ"
             height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
@@ -63,7 +63,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-XXXXXXX');
+            })(window,document,'script','dataLayer','G-Q7YEM590WZ');
           `}
         </Script>
         
